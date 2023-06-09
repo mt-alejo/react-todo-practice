245ca3f71594f6bbaac5d8e07e47a6def28d4fd7
Alejandro <mt.calderon.alejandro@gmail.com>
2023-06-08 19:43:12 -0500

    Implement useState in [search-input] JSX

--- src/TaskSearch/TaskSearch.jsx
+++ src/TaskSearch/TaskSearch.jsx
@@ -1,11 +1,20 @@
 import "./TaskSearch.css";
+import { useState } from "react";
+
 function TaskSearch() {
+  const [searchValue, setSearchValue] = useState("");
+  console.log("Search: " + searchValue);
+
   return (
     <>
       <input
         className="TaskSearch"
         type="text"
         placeholder="Write task here.."
+        value={searchValue}
+        onChange={(e) => {
+          setSearchValue(e.target.value);
+        }}
       />
     </>
   );
